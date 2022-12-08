9e71eeb091c65c9d43cc71661d433240d18a0ea5
dizmond <dizmond@live.unc.edu>
2022-12-08 12:41:07 -0500

    game works now

--- roll-dice/src/app/game/game.component.ts
+++ roll-dice/src/app/game/game.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
 export class GameComponent {
-  title = "The dice rolled a: "
-  accuracy = "100"
-  click() {
-    this.title = this.title.replace(/\d$/, '');
-    this.title += this.getRandomInt().toString();
-    this.accuracy = "50"
+  public diceRollNum: string = '';
+  gameForm = this.formBuilder.group({
+    guess: ''
+  })
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
 
   }
+  yourGuess = "Your guess was a: "
+  correctness = ""
+  title = "The dice rolled a: "
+  accuracy = "__"
+  click() {
+    console.log(this.gameForm.value.guess);
+    this.yourGuess = this.yourGuess.replace(/\d$/, '');
+    this.yourGuess += this.gameForm.value.guess;
+    this.title = this.title.replace(/\d$/, '');
+    this.diceRollNum = this.getRandomInt().toString();
+    this.title += this.diceRollNum;
+    if (this.diceRollNum == this.gameForm.value.guess) {
+      this.correctness = "Correct!";
+      console.log('correct!');
+    }
+    else {
+      this.correctness = "Wrong. Try Again!"
+    }
+  }
   getRandomInt(): number {
-    let decimal = Math.floor(Math.random() * 7);
+    let decimal = Math.floor(Math.random() * 6 + 1);
     return decimal;
   }
 
