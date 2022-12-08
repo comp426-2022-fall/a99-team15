055ef3e525c75814e6920ddeb814aecd041a53bf
dizmond <dizmond@live.unc.edu>
2022-12-08 13:39:27 -0500

    game calculates accuracy now

--- roll-dice/src/app/game/game.component.ts
+++ roll-dice/src/app/game/game.component.ts
@@ -15,11 +15,16 @@ export class GameComponent {
   ) {
 
   }
+  counter = 0;
+  numberCorrect = 0;
   yourGuess = "Your guess was a: "
   correctness = ""
   title = "The dice rolled a: "
-  accuracy = "__"
+  accuracy = 100
+  numCorrectTitle = 0;
+
   click() {
+    this.counter += 1;
     console.log(this.gameForm.value.guess);
     this.yourGuess = this.yourGuess.replace(/\d$/, '');
     this.yourGuess += this.gameForm.value.guess;
@@ -29,10 +34,12 @@ export class GameComponent {
     if (this.diceRollNum == this.gameForm.value.guess) {
       this.correctness = "Correct!";
       console.log('correct!');
+      this.numberCorrect += 1;
     }
     else {
       this.correctness = "Wrong. Try Again!"
     }
+    this.accuracy = Math.floor((this.numberCorrect / this.counter) * 100);
   }
   getRandomInt(): number {
     let decimal = Math.floor(Math.random() * 6 + 1);
